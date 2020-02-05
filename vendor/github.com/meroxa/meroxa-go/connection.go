1c94a839a1f29173ba3cd6818ac17d041c0a46d0
Ali Hamidi <ali@meroxa.io>
2020-02-05 11:41:26 -0800

    update vendor

--- vendor/github.com/meroxa/meroxa-go/connection.go
+++ vendor/github.com/meroxa/meroxa-go/connection.go
@@ -1,6 +1,11 @@
 package meroxa
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type Connector struct {
 	ID            int               `json:"id"`
@@ -13,15 +18,48 @@ type Connector struct {
 // CreateConnection provisions a connection between the Resource and the Meroxa
 // platform
 func (c *Client) CreateConnection(ctx context.Context, resourceID int, config map[string]string) (*Connector, error) {
-	return nil, nil
+	path := fmt.Sprintf("/v1/resources/%d/connection", resourceID)
+
+	resp, err := c.makeRequest(ctx, http.MethodPost, path, config)
+	if err != nil {
+		return nil, err
+	}
+
+	var con Connector
+	err = json.NewDecoder(resp.Body).Decode(&con)
+	if err != nil {
+		return nil, err
+	}
+
+	return &con, nil
 }
 
 // GetConnection returns a Connector for the given connection ID
 func (c *Client) GetConnection(ctx context.Context, id int) (*Connector, error) {
-	return nil, nil
+	path := fmt.Sprintf("/v1/connections/%d", id)
+
+	resp, err := c.makeRequest(ctx, http.MethodPost, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var con Connector
+	err = json.NewDecoder(resp.Body).Decode(&con)
+	if err != nil {
+		return nil, err
+	}
+
+	return &con, nil
 }
 
 // DeleteConnection deletes the Connector with the given id
 func (c *Client) DeleteConnection(ctx context.Context, id int) error {
+	path := fmt.Sprintf("/v1/connections/%d", id)
+
+	_, err := c.makeRequest(ctx, http.MethodDelete, path, nil)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- vendor/github.com/meroxa/meroxa-go/resource.go
+++ vendor/github.com/meroxa/meroxa-go/resource.go
@@ -13,28 +13,34 @@ type Resource struct {
 	Kind        string `json:"kind"`
 	Name        string `json:"name"`
 	URL         string `json:"url"`
-	Credentials struct {
+	Credentials *struct {
 		Username      string `json:"username"`
 		Password      string `json:"password"`
 		CACert        string `json:"ca_cert"`
 		ClientCert    string `json:"client_cert"`
 		ClientCertKey string `json:"client_cert_key"`
 		UseSSL        bool   `json:"ssl"`
-	} `json:"credentials"`
+	} `json:"credentials,omitempty"`
 	Configuration map[string]string `json:"configuration"`
 	Metadata      map[string]string `json:"metadata"`
 }
 
 // CreateResource provisions a new Resource from the given Resource struct
-func (c *Client) CreateResource(ctx context.Context, resource *Resource) error {
+func (c *Client) CreateResource(ctx context.Context, resource *Resource) (*Resource, error) {
 	path := fmt.Sprintf("/v1/resources")
 
-	_, err := c.makeRequest(ctx, http.MethodPost, path, resource)
+	resp, err := c.makeRequest(ctx, http.MethodPost, path, resource)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	var r Resource
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		return nil, err
+	}
+
+	return &r, nil
 }
 
 // ListResources returns an array of Resources (scoped to the calling user)
@@ -46,13 +52,13 @@ func (c *Client) ListResources(ctx context.Context) ([]*Resource, error) {
 		return nil, err
 	}
 
-	var res []*Resource
-	err = json.NewDecoder(resp.Body).Decode(&res)
+	var rr []*Resource
+	err = json.NewDecoder(resp.Body).Decode(&rr)
 	if err != nil {
 		return nil, err
 	}
 
-	return res, nil
+	return rr, nil
 }
 
 // GetResource returns a Resource with the given id
@@ -64,13 +70,13 @@ func (c *Client) GetResource(ctx context.Context, id int) (*Resource, error) {
 		return nil, err
 	}
 
-	var res Resource
-	err = json.NewDecoder(resp.Body).Decode(&res)
+	var r Resource
+	err = json.NewDecoder(resp.Body).Decode(&r)
 	if err != nil {
 		return nil, err
 	}
 
-	return &res, nil
+	return &r, nil
 }
 
 // DeleteResource deletes the Resource with the given id
@@ -86,6 +92,19 @@ func (c *Client) DeleteResource(ctx context.Context, id int) error {
 }
 
 // ListResourceConnections returns an array of Connectors for a given Resource
-func (c *Client) ListResourceConnections(ctx context.Context, resourceID int) ([]*Connector, error) {
-	return nil, nil
+func (c *Client) ListResourceConnections(ctx context.Context, id int) ([]*Connector, error) {
+	path := fmt.Sprintf("/v1/resources/%d/connections", id)
+
+	resp, err := c.makeRequest(ctx, http.MethodGet, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var cc []*Connector
+	err = json.NewDecoder(resp.Body).Decode(&cc)
+	if err != nil {
+		return nil, err
+	}
+
+	return cc, nil
 }
