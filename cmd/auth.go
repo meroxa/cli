574333c55614de38658519505c8730487d3129ac
Ali Hamidi <57750952+ahmeroxa@users.noreply.github.com>
2020-03-10 15:45:49 -0700

    add signup (#12)

--- cmd/auth.go
+++ cmd/auth.go
@@ -16,17 +16,56 @@ limitations under the License.
 package cmd
 
 import (
+	"bytes"
+	b64 "encoding/base64"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
+	"regexp"
 	"strings"
+	"time"
 
+	"github.com/manifoldco/promptui"
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 )
 
 const MeroxaDirPath = ".config/meroxa"
 const ConfigFileName = "meroxa.config"
+
+var signupCmd = &cobra.Command{
+	Use:   "signup",
+	Short: "sign up to the Meroxa platform",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		u, err := prompt("Username", usernameValidator, false)
+		if err != nil {
+			fmt.Println("Username invalid: ", err)
+			return err
+		}
+		p, err := prompt("Password", passwordValidator, true)
+		if err != nil {
+			fmt.Println("Password invalid: ", err)
+			return err
+		}
+		e, err := prompt("Email", emailValidator, false)
+		if err != nil {
+			fmt.Println("Email invalid: ", err)
+			return err
+		}
+
+		fmt.Println("Registering user...")
+		err = signup(u, p, e)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("User registered!")
+		return nil
+	},
+}
 
 // loginCmd represents the login command
 var loginCmd = &cobra.Command{
@@ -79,6 +118,9 @@ func init() {
 
 	// Logout
 	rootCmd.AddCommand(logoutCmd)
+
+	// Signup
+	rootCmd.AddCommand(signupCmd)
 
 	// Here you will define your flags and configuration settings.
 
@@ -157,4 +199,90 @@ func configFilePath() (string, error) {
 		return "", err
 	}
 	return mDir + "/" + ConfigFileName, nil
+}
+
+// Prompts
+
+func passwordValidator(input string) error {
+	if len(input) < 8 || len(input) > 256 {
+		return errors.New("password should be between 8 and 256 characters long")
+	}
+	return nil
+}
+
+func emailValidator(input string) error {
+	rxEmail := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+	if len(input) > 254 || !rxEmail.MatchString(input) {
+		return errors.New("email provided is invalid")
+	}
+
+	return nil
+}
+
+func usernameValidator(input string) error {
+	usernameRegexp := regexp.MustCompile(`(?i)^[a-z][a-z0-9]{2,11}$`)
+	if len(input) < 3 {
+		return errors.New("input should be at least 3 characters long")
+	}
+
+	if len(input) > 12 {
+		return errors.New("input should be no longer than 12 characters")
+	}
+
+	if !usernameRegexp.Match([]byte(input)) {
+		return errors.New("username should start only contain alphanumeric characters and start with a letter")
+	}
+
+	return nil
+}
+
+func prompt(label string, validator func(input string) error, mask bool) (string, error) {
+	p := promptui.Prompt{
+		Label:    label,
+		Validate: validator,
+	}
+
+	if mask {
+		p.Mask = '*'
+	}
+
+	return p.Run()
+}
+
+func signup(username, password, email string) error {
+	c := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	b64Password := b64.StdEncoding.EncodeToString([]byte(password))
+
+	requestBody, err := json.Marshal(map[string]string{
+		"username": username,
+		"password": b64Password,
+		"email":    email,
+	})
+	if err != nil {
+		return err
+	}
+
+	apiURL := "https://api.meroxa.io/v1/"
+	if u := os.Getenv("API_URL"); u != "" {
+		apiURL = u
+	}
+	resp, err := c.Post(apiURL+"users", "application/json", bytes.NewBuffer(requestBody))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode > 204 {
+		return fmt.Errorf("error %+v", string(body))
+	}
+
+	return nil
 }
