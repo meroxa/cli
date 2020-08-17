a5a52b718255f898025d1cfa87ff3f42f3743034
TJ Leonard <terrance.j.leonard@gmail.com>
2020-08-17 17:53:30 -0400

    Update version

--- cmd/root.go
+++ cmd/root.go
@@ -27,7 +27,7 @@ import (
 
 var cfgFile string
 
-const version = "v0.1.12"
+const version = "v0.1.13"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
