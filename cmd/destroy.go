d6de0bc6b4aae7273fe45dbeeab5ae2acd0ced0d
raulb <raulb@hey.com>
2021-01-29 22:38:19 +0100

    Update license information

--- cmd/destroy.go
+++ cmd/destroy.go
@@ -1,7 +1,7 @@
 package cmd
 
 /*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+Copyright © 2020 Meroxa Inc
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
