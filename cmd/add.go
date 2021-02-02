37fec8f5fa71b969b2e4e1baa71dd1754fcb479e
raulb <raulb@hey.com>
2021-02-02 22:36:13 +0100

    Use `stringVarP` to fetch flags

--- cmd/add.go
+++ cmd/add.go
@@ -19,129 +19,101 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+
 	"time"
 
 	"github.com/meroxa/meroxa-go"
 	"github.com/spf13/cobra"
 )
 
-func addResource(resType string, cmd *cobra.Command) error {
-	c, err := client()
+const clientTimeOut = 5 * time.Second
 
-	if err != nil {
-		return err
-	}
+var resName, resURL, resCredentials, resMetadata string
 
-	name, err := cmd.Flags().GetString("name")
-	if err != nil {
-		return err
-	}
-
-	u, err := cmd.Flags().GetString("url")
-	if err != nil {
-		return err
-	}
-
-	r := meroxa.Resource{
-		Kind: resType,
-		Name: name,
-		URL:  u,
-		// We're not doing anything with `config` in the CLI.
-		// Maybe deprecate this altogether in the client.
-		Configuration: nil,
-		Metadata:      nil,
-	}
-
-	// TODO: Figure out best way to handle creds, config and metadata
-	// Get credentials (expect a JSON string)
-	credsString, err := cmd.Flags().GetString("credentials")
-	if err != nil {
-		return err
-	}
-	if credsString != "" {
-		var creds meroxa.Credentials
-		err = json.Unmarshal([]byte(credsString), &creds)
-		if err != nil {
-			return err
-		}
-
-		r.Credentials = &creds
-	}
-
-	metadataString, err := cmd.Flags().GetString("metadata")
-	if err != nil {
-		return err
-	}
-	if metadataString != "" {
-		var metadata map[string]string
-		err = json.Unmarshal([]byte(metadataString), &metadata)
-		if err != nil {
-			return err
-		}
-
-		r.Metadata = metadata
-	}
-
-	ctx := context.Background()
-	const FIVE = 5
-	ctx, cancel := context.WithTimeout(ctx, FIVE*time.Second)
-	defer cancel()
-
-	if !flagRootOutputJSON {
-		fmt.Printf("Creating %s Resource...\n", resType)
-	}
-
-	res, err := c.CreateResource(ctx, &r)
-	if err != nil {
-		return err
-	}
-
-	if flagRootOutputJSON {
-		jsonPrint(res)
-	} else {
-		fmt.Println("Resource successfully created!")
-		prettyPrint("resource", res)
-	}
-
-	return nil
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "add meroxa resource",
+	Long:  `Use the add command to associate various Meroxa resources to your account.`,
 }
 
-func AddResourceCmd() *cobra.Command {
-	return &cobra.Command{
-		Use:   "add resource <resource-type>",
-		Short: "Add a resource to your account",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return addResource(args[0], cmd)
-		},
-	}
+var addResourceCmd = &cobra.Command{
+	Use:   "resource <resource-type>",
+	Short: "Add a resource to your account",
+	Args:  cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		resType := args[0]
+		c, err := client()
+
+		if err != nil {
+			return err
+		}
+
+		r := meroxa.Resource{
+			Kind: resType,
+			Name: resName,
+			URL:  resURL,
+			// We're not doing anything with `config` in the CLI.
+			// Maybe deprecate this altogether in the client.
+			Configuration: nil,
+			Metadata:      nil,
+		}
+
+		// TODO: Figure out best way to handle creds, config and metadata
+		// Get credentials (expect a JSON string)
+		if resCredentials != "" {
+			var creds meroxa.Credentials
+			err = json.Unmarshal([]byte(resCredentials), &creds)
+			if err != nil {
+				return err
+			}
+
+			r.Credentials = &creds
+		}
+
+		if resMetadata != "" {
+			var metadata map[string]string
+			err = json.Unmarshal([]byte(resMetadata), &metadata)
+			if err != nil {
+				return err
+			}
+
+			r.Metadata = metadata
+		}
+
+		ctx := context.Background()
+		ctx, cancel := context.WithTimeout(ctx, clientTimeOut)
+		defer cancel()
+
+		if !flagRootOutputJSON {
+			fmt.Printf("Creating %s...\n", resType)
+		}
+
+		res, err := c.CreateResource(ctx, &r)
+		if err != nil {
+			return err
+		}
+
+		if flagRootOutputJSON {
+			jsonPrint(res)
+		} else {
+			fmt.Println("Resource successfully created!")
+			prettyPrint("resource", res)
+		}
+
+		return nil
+	},
 }
 
 func init() {
-	addResourceCmd := AddResourceCmd()
+	rootCmd.AddCommand(addCmd)
+	addCmd.AddCommand(addResourceCmd)
 
-	rootCmd.AddCommand(addResourceCmd)
+	addResourceCmd.Flags().StringVarP(&resName, "name", "n", "foo", "resource name")
+	addResourceCmd.MarkFlagRequired("name")
 
-	addResourceCmd.Flags().StringP("name", "n", "", "resource name")
-	err := createConnectorCmd.MarkFlagRequired("name")
+	addResourceCmd.Flags().StringVarP(&resURL, "url", "u", "", "resource url")
+	addResourceCmd.MarkFlagRequired("url")
 
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-
-	addResourceCmd.Flags().StringP("url", "u", "", "resource url")
-
-	err = createConnectorCmd.MarkFlagRequired("url")
-
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-
-	addResourceCmd.Flags().String("credentials", "", "resource credentials")
-
-	err = createConnectorCmd.MarkFlagRequired("credentials")
-
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-	addResourceCmd.Flags().StringP("metadata", "m", "", "resource metadata")
+	addResourceCmd.Flags().StringVarP(&resCredentials, "credentials", "", "", "resource credentials")
+	addResourceCmd.Flags().StringVarP(&resMetadata, "metadata", "m", "", "resource metadata")
 }
