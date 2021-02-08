1cb8913c3f3425aeb4f8eb9a4ee3662f165f881e
Owen Ou <169064+owenthereal@users.noreply.github.com>
2021-02-08 13:41:28 -0800

    Implement `meroxa logs` (#51)
    * Implement `meroxa logs`
    
    Implement `meroxa logs`
    
    Ref: https://github.com/meroxa/platform-api/pull/157 & https://github.com/meroxa/meroxa-go/pull/10
    
    * Remove `Configuration` field
    
    Ref: https://github.com/meroxa/meroxa-go/pull/9

---
+++ cmd/log.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"context"
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(logsCmd)
+	logsCmd.AddCommand(logsConnectorCmd)
+}
+
+var logsCmd = &cobra.Command{
+	Use:   "logs",
+	Short: "Print logs for a component",
+}
+
+var logsConnectorCmd = &cobra.Command{
+	Use:   "connector <name>",
+	Short: "Print logs for a connector",
+	Args:  cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		connector := args[0]
+
+		c, err := client()
+		if err != nil {
+			return err
+		}
+
+		ctx := context.Background()
+		ctx, cancel := context.WithTimeout(ctx, clientTimeOut)
+		defer cancel()
+
+		resp, err := c.GetConnectorLogs(ctx, connector)
+		if err != nil {
+			return err
+		}
+
+		_, err = io.Copy(os.Stderr, resp.Body)
+
+		return err
+	},
+}
---
+++ vendor/github.com/meroxa/meroxa-go/log.go
@@ -0,0 +1,24 @@
+package meroxa
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+const connectorLogsBasePath = "/v1/connectors"
+
+func (c *Client) GetConnectorLogs(ctx context.Context, connectorName string) (*http.Response, error) {
+	path := fmt.Sprintf("%s/%s/logs", connectorLogsBasePath, connectorName)
+
+	req, err := c.newRequest(ctx, http.MethodGet, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Override content-type and accept headers to text/palin
+	req.Header.Add("Content-Type", textContentType)
+	req.Header.Add("Accept", textContentType)
+
+	return c.do(ctx, req)
+}
