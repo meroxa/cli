fff7cc7f580138792870fa1dcb07a30363a6e5d3
Diana Doherty <diana@meroxa.io>
2021-03-04 16:19:26 -0500

    Allow for update of pipeline status

--- cmd/update.go
+++ cmd/update.go
@@ -22,13 +22,17 @@ var updateCmd = &cobra.Command{
 }
 
 var updateConnectorCmd = &cobra.Command{
-	Use:   "connector <name> --state <pause|resume|restart>",
-	Short: "Update connector state",
-	RunE: func(cmd *cobra.Command, args []string) error {
+	Use:     "connector <name> --state <pause|resume|restart>",
+	Aliases: []string{"connectors"},
+	Short:   "Update connector state",
+	PreRunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) < 1 {
-			return errors.New("requires connector name\n\nUsage:\n  meroxa update connector <name> --state <state>")
+			return errors.New("requires connector name\n\nUsage:\n  meroxa update connector <name> --state <pause|resume|restart>")
 		}
 
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
 		// Connector Name
 		conName := args[0]
 
@@ -61,6 +65,60 @@ var updateConnectorCmd = &cobra.Command{
 	},
 }
 
+var updatePipelineCmd = &cobra.Command{
+	Use:     "pipeline <name> --state <pause|resume|restart>",
+	Aliases: []string{"pipelines"},
+	Short:   "Update pipeline state",
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("requires pipeline name\n\nUsage:\n  meroxa update pipeline <name> --state <pause|resume|restart>")
+		}
+
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// Pipeline Name
+		pipelineName := args[0]
+
+		c, err := client()
+		if err != nil {
+			return err
+		}
+
+		// get pipeline id from name
+		ctx := context.Background()
+		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+
+		pipeline, err := c.GetPipelineByName(ctx, pipelineName)
+		if err != nil {
+			return err
+		}
+
+		ctx = context.Background()
+		ctx, cancel = context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+
+		// call meroxa-go to update pipeline status with name
+		if !flagRootOutputJSON {
+			fmt.Printf("Updating %s pipeline...\n", pipelineName)
+		}
+
+		p, err := c.UpdatePipelineStatus(ctx, pipeline.ID, state)
+		if err != nil {
+			return err
+		}
+
+		if flagRootOutputJSON {
+			display.JSONPrint(p)
+		} else {
+			fmt.Printf("Pipeline %s successfully updated!\n", p.Name)
+		}
+
+		return nil
+	},
+}
+
 func init() {
 	RootCmd.AddCommand(updateCmd)
 
@@ -68,4 +126,8 @@ func init() {
 	updateCmd.AddCommand(updateConnectorCmd)
 	updateConnectorCmd.Flags().StringVarP(&state, "state", "", "", "connector state")
 	updateConnectorCmd.MarkFlagRequired("state")
+
+	updateCmd.AddCommand(updatePipelineCmd)
+	updatePipelineCmd.Flags().StringVarP(&state, "state", "", "", "pipeline state")
+	updatePipelineCmd.MarkFlagRequired("state")
 }
