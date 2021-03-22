9ffd1cc24dd6e202ed819f380ecb81e75dddd5d9
Raúl Barroso <raulb@hey.com>
2021-03-22 19:27:24 +0100

    Add test for adding a resource

---
+++ cmd/add_resource_test.go
@@ -0,0 +1,52 @@
+package cmd
+
+import (
+	"bytes"
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+func TestAddResourceCmd(t *testing.T) {
+	tests := []struct {
+		expected string
+		args []string
+	}{
+		{
+			"Error: required flag(s) \"type\", \"url\" not set",
+			[]string{"add", "resource"}},
+		{
+			"Error: required flag(s) \"type\" not set",
+			[]string{"add", "resource", "--url", "myUrl"}},
+		{
+			"Error: requires resource name",
+			[]string{"add", "resource", "--url", "myUrl", "--type", "postgres"},
+		},
+		// TODO: Add a test with resource name as argument and mocking the call
+	}
+
+	rootCmd := RootCmd()
+	b := bytes.NewBufferString("")
+	rootCmd.SetOut(b)
+	rootCmd.SetErr(b)
+
+	for _, tt := range tests {
+		rootCmd.SetArgs(tt.args)
+		rootCmd.Execute()
+		output, err := ioutil.ReadAll(b)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !strings.Contains(string(output), tt.expected) {
+			t.Fatalf("expected \"%s\" got \"%s\"", tt.expected, string(output))
+		}
+	}
+
+
+
+
+
+
+}
