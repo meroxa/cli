6ffdb2e555e1820ccd2d7bdd5465157073832c97
Raúl Barroso <raulb@hey.com>
2021-03-23 10:39:42 +0100

    Add remove pipeline test

---
+++ cmd/remove_pipeline_test.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"bytes"
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+func TestRemovePipelineCmd(t *testing.T) {
+	tests := []struct {
+		expected string
+		args []string
+	}{
+		{
+			"Error: requires pipeline name",
+			[]string{"remove", "pipeline"},
+		},
+		// TODO: Add a test mocking the call when specifying pipeline name as argument
+	}
+
+	for _, tt := range tests {
+		rootCmd := RootCmd()
+		b := bytes.NewBufferString("")
+		rootCmd.SetOut(b)
+		rootCmd.SetErr(b)
+		rootCmd.SetArgs(tt.args)
+		rootCmd.Execute()
+		output, err := ioutil.ReadAll(b)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !strings.Contains(string(output), tt.expected) {
+			t.Fatalf("expected \"%s\" got \"%s\"", tt.expected, string(output))
+		}
+	}
+}
