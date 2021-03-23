6c874f4e6ff99f78c1c5e563ae01d09add4b1264
Raúl Barroso <raulb@hey.com>
2021-03-23 12:17:48 +0100

    Separate logs connector to its own file

--- cmd/logs.go
+++ cmd/logs.go
@@ -17,13 +17,10 @@ limitations under the License.
 package cmd
 
 import (
-	"context"
-	"errors"
 	"github.com/spf13/cobra"
-	"io"
-	"os"
 )
 
+// LogsCmd represents the `meroxa logs` command
 func LogsCmd() *cobra.Command {
 	logsCmd := &cobra.Command{
 		Use:   "logs",
@@ -33,40 +30,4 @@ func LogsCmd() *cobra.Command {
 	logsCmd.AddCommand(LogsConnectorCmd())
 
 	return logsCmd
-}
-
-func LogsConnectorCmd() *cobra.Command {
-	return &cobra.Command{
-		Use:   "connector <name>",
-		Short: "Print logs for a connector",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			if len(args) < 1 {
-				return errors.New("requires connector name\n\nUsage:\n  meroxa logs connector <name>")
-			}
-			connector := args[0]
-
-			c, err := client()
-			if err != nil {
-				return err
-			}
-
-			ctx := context.Background()
-			ctx, cancel := context.WithTimeout(ctx, clientTimeOut)
-			defer cancel()
-
-			resp, err := c.GetConnectorLogs(ctx, connector)
-			if err != nil {
-				return err
-			}
-
-			_, err = io.Copy(os.Stderr, resp.Body)
-			if err != nil {
-				return err
-			}
-
-			os.Stderr.Write([]byte("\n"))
-
-			return nil
-		},
-	}
 }
