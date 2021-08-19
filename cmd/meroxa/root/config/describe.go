e90aec338c90a10dd046a38e29b5012dd178da77
Raúl Barroso <ra.barroso@gmail.com>
2021-08-19 10:44:23 +0200

    feat(config) Update docs on describe

--- cmd/meroxa/root/config/describe.go
+++ cmd/meroxa/root/config/describe.go
@@ -22,9 +22,8 @@ import (
 	"sort"
 	"strings"
 
-	"github.com/meroxa/cli/cmd/meroxa/global"
-
 	"github.com/meroxa/cli/cmd/meroxa/builder"
+	"github.com/meroxa/cli/cmd/meroxa/global"
 	"github.com/meroxa/cli/log"
 )
 
@@ -45,6 +44,16 @@ func (e *Describe) Usage() string {
 func (e *Describe) Docs() builder.Docs {
 	return builder.Docs{
 		Short: "Show Meroxa CLI configuration details",
+		Example: "" +
+			"$ meroxa config describe\n" +
+			"Using meroxa config located in \"/Users/my-name/Library/Application Support/meroxa/config.env\n\n" +
+			"access_token: c0f928b...c337a0d\n" +
+			"actor: user@email.com\n" +
+			"actor_uuid: c0f928ba-d40e-40c5-a7fa-cf281c337a0d\n" +
+			"refresh_token: c337a0d...c0f928b\n",
+		Long: "This command will return the content of your configuration file where you could find your `access_token` " +
+			"and then `refresh_token` for your `meroxa login`. They're stored in the home directory on your machine. " +
+			"On Unix, including MacOS, it's stored in `$HOME`, and on Windows is stored in `%USERPROFILE%`.",
 	}
 }
 
--- docs/cmd/www/meroxa-config-describe.md
+++ docs/cmd/www/meroxa-config-describe.md
@@ -9,8 +9,25 @@ url: /cli/cmd/meroxa-config-describe/
 
 Show Meroxa CLI configuration details
 
+### Synopsis
+
+This command will return the content of your configuration file where you could find your `access_token` and then `refresh_token` for your `meroxa login`. They're stored in the home directory on your machine. On Unix, including MacOS, it's stored in `$HOME`, and on Windows is stored in `%USERPROFILE%`.
+
 ```
 meroxa config describe [flags]
+```
+
+### Examples
+
+```
+$ meroxa config describe
+Using meroxa config located in "/Users/my-name/Library/Application Support/meroxa/config.env
+
+access_token: c0f928b...c337a0d
+actor: user@email.com
+actor_uuid: c0f928ba-d40e-40c5-a7fa-cf281c337a0d
+refresh_token: c337a0d...c0f928b
+
 ```
 
 ### Options
