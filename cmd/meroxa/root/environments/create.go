1b75360e1b71b368371af83db06c93c690db7570
Raúl Barroso <raul@meroxa.io>
2021-10-28 17:21:21 +0200

    chore: fix doc on env create (#206)

--- cmd/meroxa/root/environments/create.go
+++ cmd/meroxa/root/environments/create.go
@@ -54,7 +54,7 @@ type Create struct {
 		Type     string `long:"type" usage:"environment type, when not specified"`
 		Provider string `long:"provider" usage:"environment cloud provider to use"`
 		Region   string `long:"region" usage:"environment region"`
-		Config   string `short:"c" long:"config" usage:"environment configuration based on type and provider (e.g.: --config {\"aws_access_key_id\":\"my_access_key\", \"aws_access_secret\":\"my_access_secret\"})"` // nolint:lll
+		Config   string `short:"c" long:"config" usage:"environment configuration based on type and provider (e.g.: --config '{\"aws_access_key_id\":\"my_access_key\", \"aws_secret_access_key\":\"my_secret_access_key\"}')"` // nolint:lll
 	}
 
 	envCfg map[string]interface{}
@@ -268,7 +268,11 @@ func (c *Create) Docs() builder.Docs {
 	return builder.Docs{
 		Short: "Create an environment",
 		Example: `
-meroxa env create my-env --type hosted --provider aws --region us-east-1 --config aws_access_key_id=1234 --config aws_secret_access_key=5678
+meroxa env create my-env \
+	--type hosted \ 
+	--provider aws \ 
+	--region us-east-1 \ 
+	--config '{\"aws_access_key_id\":\"my_access_key\", \"aws_secret_access_key\":\"my_secret_access_key\"}'
 `,
 	}
 }
