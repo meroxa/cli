cd749853ed244ef8f065f78fdf643980f8dd99a1
Owen Ou <169064+owenthereal@users.noreply.github.com>
2022-02-14 15:36:47 -0800

    Add `meroxa functions log` (#258)
    * Add `meroxa functions log`
    
    Add `meroxa functions log`. This is part of
    https://github.com/meroxa/cli/issues/244.
    
    * Update cmd/meroxa/root/functions/logs.go
    
    Co-authored-by: Diana Doherty <31331000+dianadoherty@users.noreply.github.com>
    
    * Update cmd/meroxa/root/functions/logs.go
    
    Co-authored-by: Diana Doherty <31331000+dianadoherty@users.noreply.github.com>
    
    * More uuid names
    
    Co-authored-by: Diana Doherty <31331000+dianadoherty@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -13,7 +13,7 @@ require (
 	github.com/manifoldco/promptui v0.8.0
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-runewidth v0.0.10 // indirect
-	github.com/meroxa/meroxa-go v0.0.0-20220208195203-71ddc3133fab
+	github.com/meroxa/meroxa-go v0.0.0-20220214221023-2da2e3a247a7
 	github.com/nirasan/go-oauth-pkce-code-verifier v0.0.0-20170819232839-0fbfe93532da
 	github.com/pkg/browser v0.0.0-20210115035449-ce105d075bb4
 	github.com/rivo/uniseg v0.2.0 // indirect
--- vendor/github.com/meroxa/meroxa-go/pkg/meroxa/log.go
+++ vendor/github.com/meroxa/meroxa-go/pkg/meroxa/log.go
@@ -6,10 +6,28 @@ import (
 	"net/http"
 )
 
-const connectorLogsBasePath = "/v1/connectors"
+const (
+	connectorLogsBasePath = "/v1/connectors"
+	functionLogsBasePath  = "/v1/functions"
+)
 
 func (c *client) GetConnectorLogs(ctx context.Context, nameOrID string) (*http.Response, error) {
 	path := fmt.Sprintf("%s/%s/logs", connectorLogsBasePath, nameOrID)
+
+	req, err := c.newRequest(ctx, http.MethodGet, path, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Override content-type and accept headers to text/palin
+	req.Header.Add("Content-Type", textContentType)
+	req.Header.Add("Accept", textContentType)
+
+	return c.httpClient.Do(req)
+}
+
+func (c *client) GetFunctionLogs(ctx context.Context, nameOrID string) (*http.Response, error) {
+	path := fmt.Sprintf("%s/%s/logs", functionLogsBasePath, nameOrID)
 
 	req, err := c.newRequest(ctx, http.MethodGet, path, nil, nil)
 	if err != nil {
