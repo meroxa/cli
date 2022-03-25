b4c92f58573628594f4e7ea5351fabb20f27ffb3
Janelle Tavares <janelletavares@users.noreply.github.com>
2022-03-25 14:46:14 -0700

    bug(turbine): Adjust to new pipeline log (#285)
    * Adjust to pipeline log reformat and updated regex for valid app names

--- cmd/meroxa/turbine_cli/utils.go
+++ cmd/meroxa/turbine_cli/utils.go
@@ -123,7 +123,7 @@ func GitChecks(ctx context.Context, l log.Logger, appPath string) error {
 func GetPipelineUUID(output string) string {
 	// Example output:
 	// 2022/03/16 13:21:36 pipeline created: "turbine-pipeline-simple" ("049760a8-a3d2-44d9-b326-0614c09a3f3e").
-	re := regexp.MustCompile(`pipeline created:."[a-zA-Z]+-[a-zA-Z]+-[a-zA-Z]+".(\([^)]*\))`)
+	re := regexp.MustCompile(`pipeline:."turbine-pipeline-[a-z0-9-]+".(\([^)]*\))`)
 	res := re.FindStringSubmatch(output)[1]
 	res = strings.Trim(res, "()\"")
 	return res
