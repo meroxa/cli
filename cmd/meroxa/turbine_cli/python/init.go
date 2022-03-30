ea65bc47a38895ea4a92ba5d6933b436672fd017
janelletavares <janelle@meroxa.io>
2022-03-30 16:34:26 -0700

    new python executable

--- cmd/meroxa/turbine_cli/python/init.go
+++ cmd/meroxa/turbine_cli/python/init.go
@@ -9,8 +9,7 @@ import (
 )
 
 func Init(ctx context.Context, l log.Logger, name, path string) error {
-	// @TODO sort out binary names so there's no clashing
-	cmd := exec.Command("$HOME/.local/bin/turbine", "--generate", name, path)
+	cmd := exec.Command("turbine-py", "--generate", name, path)
 	_, err := turbineCLI.RunCmdWithErrorDetection(ctx, cmd, l)
 	return err
 }
