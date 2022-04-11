8a8022e65330d876caa209f1ce8714ad8f2e25aa
janelletavares <janelle@meroxa.io>
2022-04-11 16:18:02 -0700

    feat(apps): Update python commands

--- cmd/meroxa/turbine_cli/python/deploy.go
+++ cmd/meroxa/turbine_cli/python/deploy.go
@@ -1,8 +1,27 @@
 package turbinepy
 
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
 // TODO: Add a function that creates the needed structure for a python app
 
-// TODO: Add a function to return whether the app has functions or not
+// NeedsToBuild determines if the app has functions or not
+func NeedsToBuild(path string) (bool, error) {
+	cmd := exec.Command("turbine-py", "hasFunctions", path)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		err := fmt.Errorf(
+			"unable to determine if the Meroxa Application at %s has a Process; %s",
+			path,
+			string(output))
+		return false, err
+	}
+	return strconv.ParseBool(strings.TrimSpace(string(output)))
+}
 
 // TODO: Add a function that actually creates the meroxa resources...
 
