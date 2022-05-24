8933d9166af5c344929c65e23a6ef845052da12a
meroxa-machine <70029809+meroxa-machine@users.noreply.github.com>
2022-05-24 16:26:27 +0200

    chore: Upgrade to latest turbine-go (#361)
    Co-authored-by: github-actions <github-actions@github.com>

--- vendor/github.com/meroxa/turbine-go/init/template/app.go
+++ vendor/github.com/meroxa/turbine-go/init/template/app.go
@@ -22,13 +22,14 @@ type App struct{}
 
 func (a App) Run(v turbine.Turbine) error {
 	// To configure your data stores as resources on the Meroxa Platform
-	// use the Meroxa Dashboard, CLI, or Meroxa Terraform Provider
+	// use the Meroxa Dashboard, CLI, or Meroxa Terraform Provider.
 	// For more details refer to: https://docs.meroxa.com/
-
+	//
 	// Identify an upstream data store for your data app
 	// with the `Resources` function
 	// Replace `source_name` with the resource name the
-	// data store was configured with on Meroxa
+	// data store was configured with on Meroxa.
+
 	source, err := v.Resources("source_name")
 	if err != nil {
 		return err
@@ -37,7 +38,12 @@ func (a App) Run(v turbine.Turbine) error {
 	// Specify which upstream records to pull
 	// with the `Records` function
 	// Replace `collection_name` with a table, collection,
-	// or bucket name in your data store
+	// or bucket name in your data store.
+	// If a configuration is needed for your source,
+	// you can pass it as a second argument to the `Records` function. For example:
+	//
+	// source.Records("collection_name", map[string]interface{"incrementing.field.name":"id"})
+
 	rr, err := source.Records("collection_name", nil)
 	if err != nil {
 		return err
@@ -45,13 +51,15 @@ func (a App) Run(v turbine.Turbine) error {
 
 	// Specify what code to execute against upstream records
 	// with the `Process` function
-	// Replace `Anonymize` with the name of your function code
-	res, _ := v.Process(rr, Anonymize{})
+	// Replace `Anonymize` with the name of your function code.
+
+	res := v.Process(rr, Anonymize{})
 
 	// Identify a downstream data store for your data app
 	// with the `Resources` function
 	// Replace `destination_name` with the resource name the
-	// data store was configured with on Meroxa
+	// data store was configured with on Meroxa.
+
 	dest, err := v.Resources("destination_name")
 	if err != nil {
 		return err
@@ -60,9 +68,15 @@ func (a App) Run(v turbine.Turbine) error {
 	// Specify where to write records downstream
 	// using the `Write` function
 	// Replace `collection_archive` with a table, collection,
-	// or bucket name in your data store
-	// if a configuration is needed you can also use
-	// dest.WriteWithConfig
+	// or bucket name in your data store.
+	// If a configuration is needed, you can also use i.e.
+	//
+	// dest.WriteWithConfig(
+	//  res,
+	//  "my-archive",
+	//  map[string]interface{"buffer.flush.time", Value: "10"}
+	// )
+
 	err = dest.Write(res, "collection_archive")
 	if err != nil {
 		return err
@@ -73,22 +87,22 @@ func (a App) Run(v turbine.Turbine) error {
 
 type Anonymize struct{}
 
-func (f Anonymize) Process(stream []turbine.Record) ([]turbine.Record, []turbine.RecordWithError) {
-	for i, r := range stream {
-		e := fmt.Sprintf("%s", r.Payload.Get("after.customer_email"))
-		if e == "" {
-			log.Printf("unable to find customer_email value in %d record\n", i)
+func (f Anonymize) Process(stream []turbine.Record) []turbine.Record {
+	for i, record := range stream {
+		email := fmt.Sprintf("%s", record.Payload.Get("after.customer_email"))
+		if email == "" {
+			log.Printf("unable to find customer_email value in record %d\n", i)
 			break
 		}
-		hashedEmail := consistentHash(e)
-		err := r.Payload.Set("after.customer_email", hashedEmail)
+		hashedEmail := consistentHash(email)
+		err := record.Payload.Set("after.customer_email", hashedEmail)
 		if err != nil {
 			log.Println("error setting value: ", err)
-			break
+			continue
 		}
-		stream[i] = r
+		stream[i] = record
 	}
-	return stream, nil
+	return stream
 }
 
 func consistentHash(s string) string {
