b2b125e5320e7b819da8eb3334ea198049d6cc26
Jesse Jordan <jesse@meroxa.io>
2022-05-25 13:51:58 +0200

    feat(deploy): check for resource availability before app deployments
    Co-authored by Janelle Tavares <janelle@meroxa.io>
    Co-authored by Raul Barosso <raul@meroxa.io>

--- cmd/meroxa/root/apps/deploy.go
+++ cmd/meroxa/root/apps/deploy.go
@@ -525,40 +525,15 @@ func (d *Deploy) validate(ctx context.Context) error {
 	return turbineCLI.ValidateBranch(ctx, d.logger, d.path)
 }
 
-func (d *Deploy) checkResourceAvailability(ctx context.Context) error {
-	d.logger.Infof(ctx, "Checking resource availability for application %q (%s) before deployment...", d.appName, d.lang)
-
-	var resourceNames []string
-	var err error
-	switch d.lang {
-	case GoLang:
-		resourceNames, err = turbineGo.GetResourceNames(ctx, d.logger, d.path, d.appName)
-	case JavaScript, NodeJs:
-		resourceNames, err = turbineJS.GetResourceNames(ctx, d.logger, d.path, d.appName)
-	case Python:
-		resourceNames, err = turbinePY.GetResourceNames(ctx, d.logger, d.path, d.appName)
-	default:
-		return fmt.Errorf("language %q not supported. %s", d.lang, LanguageNotSupportedError)
-	}
-	if err != nil {
-		d.logger.Warnf(ctx, "Unable to check for resource availability (%s). Moving on...", err.Error())
-		return nil
-	}
-	if len(resourceNames) == 0 {
-		// Turbine bug or misconfigured app.
-		d.logger.Warnf(ctx, "Unable to check for resource availability. Moving on...")
-		return nil
-	}
-
+func (d *Deploy) getResourceCheckErrorMessage(ctx context.Context, resourceNames []string) string {
 	var errStr string
 	for _, name := range resourceNames {
 		resource, err := d.client.GetResourceByNameOrID(ctx, name)
 		if err != nil {
 			if errStr != "" {
 				errStr += "; "
-			} else {
-				errStr = "run 'meroxa resource list' to double check the name and try again; "
 			}
+
 			if strings.Contains(err.Error(), "could not find") {
 				errStr += fmt.Sprintf("could not find resource %q", name)
 			} else {
@@ -571,23 +546,58 @@ func (d *Deploy) checkResourceAvailability(ctx context.Context) error {
 			errStr += fmt.Sprintf("resource %q is not ready and usable", resource.Name)
 		}
 	}
+	return errStr
+}
+
+func (d *Deploy) checkResourceAvailability(ctx context.Context) error {
+	resourceCheckMessage := fmt.Sprintf("Checking resource availability for application %q (%s) before deployment...", d.appName, d.lang)
+
+	d.logger.StartSpinner("\t", resourceCheckMessage)
+
+	var resourceNames []string
+	var err error
+
+	switch d.lang {
+	case GoLang:
+		resourceNames, err = turbineGo.GetResourceNames(ctx, d.logger, d.path, d.appName)
+	case JavaScript:
+		resourceNames, err = turbineJS.GetResourceNames(ctx, d.logger, d.path, d.appName)
+	case Python:
+		resourceNames, err = turbinePY.GetResourceNames(ctx, d.logger, d.path, d.appName)
+	}
+
+	if err != nil {
+		return fmt.Errorf("unable to read resource definition from app: %s", err.Error())
+	}
+
+	if len(resourceNames) == 0 {
+		return errors.New("no resources defined in your Turbine app")
+	}
+
+	errStr := d.getResourceCheckErrorMessage(ctx, resourceNames)
+
 	if errStr != "" {
+		errStr += ";\n\n ⚠️  Run 'meroxa resource list' to verify that the names of resources " +
+			"defined in your Turbine app are identical to the resources you have created on the Meroxa platform before deploying again"
+		d.logger.StopSpinnerWithStatus("Resource availability check failed", log.Failed)
 		return fmt.Errorf("%s", errStr)
 	}
+
+	d.logger.StopSpinnerWithStatus("Found all required resources!", log.Successful)
 	return nil
 }
 
 func (d *Deploy) prepareAppForDeployment(ctx context.Context) error {
 	d.logger.Infof(ctx, "Preparing application %q (%s) for deployment...", d.appName, d.lang)
 
-	// check that resources exist and are ready
-	err := d.checkResourceAvailability(ctx)
+	// After this point, CLI will package it up and will build it
+	err := d.buildApp(ctx)
 	if err != nil {
 		return err
 	}
 
-	// After this point, CLI will package it up and will build it
-	err = d.buildApp(ctx)
+	// check that resources exist and are ready
+	err = d.checkResourceAvailability(ctx)
 	if err != nil {
 		return err
 	}
