c595a10b046c6022a28ce70c2f86fc7b2ca57809
Jesse Jordan <jesse@meroxa.io>
2022-06-01 21:23:59 +0200

    deps(turbine-js): bump turbine-js to latest version

--- cmd/meroxa/turbine_cli/utils.go
+++ cmd/meroxa/turbine_cli/utils.go
@@ -20,7 +20,7 @@ import (
 	"github.com/meroxa/cli/log"
 )
 
-const turbineJSVersion = "0.1.7"
+const turbineJSVersion = "0.1.8"
 
 type AppConfig struct {
 	Name        string            `json:"name"`
