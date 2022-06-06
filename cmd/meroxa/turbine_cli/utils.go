e9dddf08b24db1b405007cb3e2f1660c9e2e0e5f
James Martinez <jmar910@gmail.com>
2022-06-06 15:24:50 -0400

    Update turbine-js to v0.2.1

--- cmd/meroxa/turbine_cli/utils.go
+++ cmd/meroxa/turbine_cli/utils.go
@@ -20,7 +20,7 @@ import (
 	"github.com/meroxa/cli/log"
 )
 
-const turbineJSVersion = "0.1.8"
+const turbineJSVersion = "0.2.1"
 
 type AppConfig struct {
 	Name        string            `json:"name"`
