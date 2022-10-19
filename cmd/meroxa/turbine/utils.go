1a78511b4880adbcc8fc8e87dd18a8ad6a7cc411
James Martinez <jmar910@gmail.com>
2022-10-19 17:11:50 -0400

    fix(turbine-js): bump to 1.3.1

--- cmd/meroxa/turbine/utils.go
+++ cmd/meroxa/turbine/utils.go
@@ -29,7 +29,7 @@ const (
 	Python     = "python"
 	Python3    = "python3"
 
-	turbineJSVersion  = "1.3.0"
+	turbineJSVersion  = "1.3.1"
 	isTrue            = "true"
 	AccountUUIDEnvVar = "MEROXA_ACCOUNT_UUID"
 )
