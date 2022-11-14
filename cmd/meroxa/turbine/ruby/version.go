8567004a459b279e09894fd696aecec1e98e6500
Lyubo Kamenov <lyubo@meroxa.io>
2022-11-14 15:56:19 -0500

    Update version.go
    Add ruby

--- cmd/meroxa/turbine/ruby/version.go
+++ cmd/meroxa/turbine/ruby/version.go
@@ -7,5 +7,18 @@ import (
 
 // GetVersion will return the version of turbine-rb dependency of a given app.
 func (t *turbineRbCLI) GetVersion(ctx context.Context) (string, error) {
-	return "", fmt.Errorf("unimplemented")
+	cmd := exec.Command("ruby", []string{
+		"-r", "rubygems",
+		"-r", fmt.Sprintf("%s/app", t.appPath),
+		"-e", `puts Gem.loaded_specs["turbine"].version.version`
+	})
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		err = fmt.Errorf(
+			"unable to determine the turbine-rb version of the Meroxa Application at %s",
+			t.appPath)
+		return "", err	
+	}
+	
+	return strings.TrimSpace(string(output)), nil
 }
