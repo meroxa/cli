ebe7a011da0aaebab903af14c44c4bfc99386270
janelletavares <janelle@meroxa.io>
2022-11-14 12:42:08 -0800

    ruby stub

---
+++ cmd/meroxa/turbine/ruby/version.go
@@ -0,0 +1,11 @@
+package turbinerb
+
+import (
+	"context"
+	"fmt"
+)
+
+// GetVersion will return the version of turbine-rb dependency of a given app.
+func (t *turbineRbCLI) GetVersion(ctx context.Context) (string, error) {
+	return "", fmt.Errorf("unimplemented")
+}
