ad20f12156b2676ce382c460a197fa2b4476ad5b
janelletavares <janelle@meroxa.io>
2022-11-16 10:02:41 -0800

    Bump turbine-js-cli version to 1.3.4

--- cmd/meroxa/turbine/utils.go
+++ cmd/meroxa/turbine/utils.go
@@ -30,7 +30,7 @@ const (
 	Python3    = "python3"
 	Ruby       = "ruby"
 
-	turbineJSVersion  = "1.3.3"
+	turbineJSVersion  = "1.3.4"
 	isTrue            = "true"
 	AccountUUIDEnvVar = "MEROXA_ACCOUNT_UUID"
 
