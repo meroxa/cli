8bd90535b3a0f205e5c01b4797047a9ba9cfc0bb
Janelle Tavares <janelletavares@users.noreply.github.com>
2022-12-16 14:34:53 -0800

    Git init failing (#581)
    * Git init failing

--- cmd/meroxa/turbine/utils.go
+++ cmd/meroxa/turbine/utils.go
@@ -189,25 +189,24 @@ func GitInit(ctx context.Context, appPath string) error {
 		return err
 	}
 
-	if !isGitOlderThan228 {
-		cmd := exec.CommandContext(ctx, "git", "config", "--global", "init.defaultBranch", "main")
-		if out, err := cmd.CombinedOutput(); err != nil {
-			return errors.New(string(out))
-		}
-	}
-
 	cmd := exec.CommandContext(ctx, "git", "init", appPath)
 	if out, err := cmd.CombinedOutput(); err != nil {
 		return fmt.Errorf(string(out))
 	}
 
-	if isGitOlderThan228 {
-		cmd := exec.CommandContext(ctx, "git", "checkout", "-b", "main")
+	if !isGitOlderThan228 {
+		cmd = exec.CommandContext(ctx, "git", "config", "init.defaultBranch", "main")
 		cmd.Dir = appPath
 		if out, err := cmd.CombinedOutput(); err != nil {
 			return errors.New(string(out))
 		}
 	}
+	cmd = exec.CommandContext(ctx, "git", "checkout", "-b", "main")
+	cmd.Dir = appPath
+	if out, err := cmd.CombinedOutput(); err != nil {
+		return errors.New(string(out))
+	}
+
 	return nil
 }
 
