cd74e39de14afa03967b2933e3a722d41fc70ef6
Raúl Barroso <ra.barroso@gmail.com>
2023-01-27 12:54:01 +0100

    feat: Include environment information on apps describe
    - Depends on https://github.com/meroxa/meroxa-go/pull/157 and it's branched off https://github.com/meroxa/cli/pull/603

--- utils/display/apps_test.go
+++ utils/display/apps_test.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/meroxa/cli/utils"
+
 	"github.com/meroxa/meroxa-go/pkg/meroxa"
 )
 
@@ -37,5 +39,85 @@ func TestAppLogsTable(t *testing.T) {
 	}
 	if !strings.Contains(out, fmt.Sprintf("# Logs for %s deployment\n\n%s", deploymentUUID, log)) {
 		t.Errorf("expected %q to be shown with logs", deploymentUUID)
+	}
+}
+
+func TestAppDescribeTable(t *testing.T) {
+	testCases := []struct {
+		desc                 string
+		app                  func() *meroxa.Application
+		shouldErrorOnEnvInfo func(string) bool
+	}{
+		{
+			desc: "Application with no environment",
+			app: func() *meroxa.Application {
+				a := utils.GenerateApplication("")
+				return &a
+			},
+			shouldErrorOnEnvInfo: func(output string) bool {
+				return strings.Contains(output, "Environment")
+			},
+		},
+		{
+			desc: "Application with a common environment",
+			app: func() *meroxa.Application {
+				a := utils.GenerateApplicationWithEnv("", meroxa.EnvironmentTypeCommon, "")
+				return &a
+			},
+			shouldErrorOnEnvInfo: func(output string) bool {
+				return strings.Contains(output, "Environment")
+			},
+		},
+		{
+			desc: "Application with in a private environment",
+			app: func() *meroxa.Application {
+				a := utils.GenerateApplicationWithEnv("", meroxa.EnvironmentTypePrivate, meroxa.EnvironmentProviderAws)
+				return &a
+			},
+			shouldErrorOnEnvInfo: func(output string) bool {
+				return !strings.Contains(output, "Environment")
+			},
+		},
+		{
+			desc: "Application with in a private environment",
+			app: func() *meroxa.Application {
+				a := utils.GenerateApplicationWithEnv("", meroxa.EnvironmentTypeSelfHosted, "")
+				return &a
+			},
+			shouldErrorOnEnvInfo: func(output string) bool {
+				return !strings.Contains(output, "Environment")
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			app := tc.app()
+			out := AppTable(app)
+			if !strings.Contains(out, "UUID") {
+				t.Errorf("expected %q to be shown\n%s\n", app.UUID, out)
+			}
+			if !strings.Contains(out, "Name:") {
+				t.Errorf("expected %q to be shown\n%s\n", app.Name, out)
+			}
+			if !strings.Contains(out, "Language:") {
+				t.Errorf("expected %q to be shown\n%s\n", app.Language, out)
+			}
+			if !strings.Contains(out, "Git SHA:") {
+				t.Errorf("expected %q to be shown\n%s\n", app.GitSha, out)
+			}
+			if !strings.Contains(out, "Created At:") {
+				t.Errorf("expected %q to be shown\n%s\n", app.CreatedAt, out)
+			}
+			if !strings.Contains(out, "Updated At:") {
+				t.Errorf("expected %q to be shown\n%s\n", app.UpdatedAt, out)
+			}
+			if !strings.Contains(out, "State:") {
+				t.Errorf("expected %q to be shown\n%s\n", app.Status.State, out)
+			}
+			if tc.shouldErrorOnEnvInfo(out) {
+				t.Errorf("expected environment information to be shown\n%s\n", out)
+			}
+		})
 	}
 }
