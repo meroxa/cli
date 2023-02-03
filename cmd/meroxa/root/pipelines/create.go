7cd07da8fe8958252c6e6ac5047595ad9da01f30
Diana Doherty <diana@meroxa.io>
2023-02-03 17:08:18 -0500

    Remove cli logic to handle common env for api

--- cmd/meroxa/root/pipelines/create.go
+++ cmd/meroxa/root/pipelines/create.go
@@ -76,8 +76,7 @@ func (c *Create) Execute(ctx context.Context) error {
 		p.Metadata = metadata
 	}
 
-	// If the environment specified is not the common environment.
-	if c.flags.Environment != "" && c.flags.Environment != string(meroxa.EnvironmentTypeCommon) {
+	if c.flags.Environment != "" {
 		err := builder.CheckCMDFeatureFlag(c, &environments.Environments{})
 		if err != nil {
 			return err
