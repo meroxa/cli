ff9809961dda71293fd5f727051eaa0e35fe19e7
James Martinez <jmar910@gmail.com>
2023-04-26 12:22:40 -0400

    feat: turbine-js apps init via turbine-core

--- cmd/meroxa/turbine/javascript/init.go
+++ cmd/meroxa/turbine/javascript/init.go
@@ -2,15 +2,45 @@ package turbinejs
 
 import (
 	"context"
+	"os"
+	"os/exec"
 
 	utils "github.com/meroxa/cli/cmd/meroxa/turbine"
+	"github.com/meroxa/turbine-core/pkg/app"
+	"github.com/meroxa/turbine-core/pkg/ir"
 )
 
-func (t *turbineJsCLI) Init(ctx context.Context, name string) error {
-	_, err := RunTurbineJS(ctx, t.logger, "generate", name, t.appPath)
-	return err
+func (t *turbineJsCLI) Init(_ context.Context, appName string) error {
+	err := app.NewAppInit(appName, ir.JavaScript, t.appPath).Init()
+	if err != nil {
+		return err
+	}
+
+	err = jsInit(t.appPath + "/" + appName)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (t *turbineJsCLI) GitInit(ctx context.Context, name string) error {
 	return utils.GitInit(ctx, name)
+}
+
+func jsInit(appPath string) error {
+	// temporarily switching to the app's directory
+	pwd, err := utils.SwitchToAppDirectory(appPath)
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command("npm", "install")
+	_, err = cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+
+	return os.Chdir(pwd)
 }
