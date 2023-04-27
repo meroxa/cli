a71d209af20e8b539f763ac0bc85944045269092
anna-cross <annakh@meroxa.io>
2023-04-27 10:59:25 -0400

    Draft of the run command changes for the turbine-core implement for turbine-py

--- cmd/meroxa/turbine/python/cli.go
+++ cmd/meroxa/turbine/python/cli.go
@@ -3,14 +3,26 @@ package turbinepy
 import (
 	"github.com/meroxa/cli/cmd/meroxa/turbine"
 	"github.com/meroxa/cli/log"
+	"github.com/meroxa/turbine-core/pkg/client"
+	"github.com/meroxa/turbine-core/pkg/server"
 )
 
 type turbinePyCLI struct {
-	logger  log.Logger
-	appPath string
-	tmpPath string
+	logger            log.Logger
+	appPath           string
+	tmpPath           string
+	grpcListenAddress string
+	runner            server.Server
+	builder           server.Server
+	bc                client.Client
 }
 
 func New(l log.Logger, appPath string) turbine.CLI {
-	return &turbinePyCLI{logger: l, appPath: appPath}
+	return &turbinePyCLI{
+		logger:            l,
+		appPath:           appPath,
+		grpcListenAddress: server.ListenAddress,
+		runner:            server.NewRunServer(),
+		builder:           server.NewSpecBuilderServer(),
+	}
 }
