9555c64b85a8aaf4d129e97ec1cb37d6630bf9de
anna-cross <annakh@meroxa.io>
2023-05-04 10:47:20 -0400

    Add deploy to turbine-py for core

--- cmd/meroxa/turbine/python/cli.go
+++ cmd/meroxa/turbine/python/cli.go
@@ -2,15 +2,27 @@ package turbinepy
 
 import (
 	"github.com/meroxa/cli/cmd/meroxa/turbine"
+	"github.com/meroxa/cli/cmd/meroxa/turbine/internal"
 	"github.com/meroxa/cli/log"
+	"github.com/meroxa/turbine-core/pkg/client"
+	"github.com/meroxa/turbine-core/pkg/server"
 )
 
 type turbinePyCLI struct {
-	logger  log.Logger
-	appPath string
-	tmpPath string
+	logger            log.Logger
+	appPath           string
+	grpcListenAddress string
+	runner            server.Server
+	builder           server.Server
+	bc                client.Client
 }
 
 func New(l log.Logger, appPath string) turbine.CLI {
-	return &turbinePyCLI{logger: l, appPath: appPath}
+	return &turbinePyCLI{
+		logger:            l,
+		appPath:           appPath,
+		grpcListenAddress: internal.RandomLocalAddr(),
+		runner:            server.NewRunServer(),
+		builder:           server.NewSpecBuilderServer(),
+	}
 }
--- cmd/meroxa/turbine/python/run.go
+++ cmd/meroxa/turbine/python/run.go
@@ -2,14 +2,19 @@ package turbinepy
 
 import (
 	"context"
-	"os/exec"
 
-	utils "github.com/meroxa/cli/cmd/meroxa/turbine"
+	"github.com/meroxa/cli/cmd/meroxa/turbine"
+	"github.com/meroxa/cli/cmd/meroxa/turbine/python/internal"
 )
 
 func (t *turbinePyCLI) Run(ctx context.Context) error {
-	cmd := exec.Command("turbine-py", "run", t.appPath)
-	stdOut, err := utils.RunCmdWithErrorDetection(ctx, cmd, t.logger)
-	t.logger.Info(ctx, stdOut)
-	return err
+	go t.runner.Run(ctx)
+	defer t.runner.GracefulStop()
+	cmd := internal.NewTurbineCmd(t.appPath,
+		internal.TurbineCommandRun,
+		map[string]string{
+			"TURBINE_CORE_SERVER": t.grpcListenAddress,
+		},
+		t.appPath)
+	return turbine.RunCMD(ctx, t.logger, cmd)
 }
