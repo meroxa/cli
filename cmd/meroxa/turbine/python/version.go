508e3c9e7f46e1c5ca90f3b8217361fada0cf9da
Simon Lawrence <simon@meroxa.io>
2023-05-05 12:11:48 -0400

    Fix for turbine-py output not being trimmed.
    This fixes the path dependent issue where the CLI output was not getting
    trimmed on the code path were an error didn't occur.

--- cmd/meroxa/turbine/python/version.go
+++ cmd/meroxa/turbine/python/version.go
@@ -21,12 +21,13 @@ func (t *turbinePyCLI) GetVersion(_ context.Context) (string, error) {
 	}
 
 	version, err := utils.GetTurbineResponseFromOutput(string(output))
+	// FYI the error return here is being used to fallback to previous CLI output format
 	if err != nil {
 		// For versions <= v1.6.1
 		version = string(output)
-		version = strings.TrimSpace(version)  // it will remove tab '\t' and new lines '\n' as well
-		version = strings.Trim(version, "\r") // windows: removing part of windows new line
+		version = strings.TrimSpace(version) // it will remove tab '\t' and new lines '\n' as well
 		return version, nil
 	}
+
 	return version, err
 }
