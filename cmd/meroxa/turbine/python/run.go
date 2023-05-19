1056d5ccbd060662483ccdd0a9d083d02db8d76d
anna-cross <annakh@meroxa.io>
2023-05-19 10:39:57 -0400

    fix: cleanup turbine-py related items

--- cmd/meroxa/turbine/python/run.go
+++ cmd/meroxa/turbine/python/run.go
@@ -10,17 +10,13 @@ import (
 func (t *turbinePyCLI) Run(ctx context.Context) error {
 	go t.runner.RunAddr(ctx, t.grpcListenAddress)
 	defer t.runner.GracefulStop()
-	gitSha, err := t.GetGitSha(ctx)
-	if err != nil {
-		return err
-	}
 	cmd := internal.NewTurbineCmd(t.appPath,
 		internal.TurbineCommandRun,
 		map[string]string{
 			"TURBINE_CORE_SERVER": t.grpcListenAddress,
 		},
 		t.appPath,
-		gitSha,
+		"git_sha",
 	)
 	return turbine.RunCMD(ctx, t.logger, cmd)
 }
