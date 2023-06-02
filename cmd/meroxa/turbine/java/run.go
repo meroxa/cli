f74c1c910980b56aef3091ad19f194df68344aa8
Haris Osmanagic <haris@meroxa.io>
2023-06-02 17:41:37 +0200

    Hack for building and running Java apps

--- cmd/meroxa/turbine/java/run.go
+++ cmd/meroxa/turbine/java/run.go
@@ -2,10 +2,11 @@ package turbinejava
 
 import (
 	"context"
+	"fmt"
+	"github.com/meroxa/cli/cmd/meroxa/turbine"
 	"os"
 	"os/exec"
-
-	"github.com/meroxa/cli/cmd/meroxa/turbine"
+	"strings"
 )
 
 func (t *turbineJavaCLI) Run(ctx context.Context) error {
@@ -13,19 +14,56 @@ func (t *turbineJavaCLI) Run(ctx context.Context) error {
 	defer gracefulStop()
 
 	// TODO: here it goes the command specific in java to run the gRPC server
-	//cmd := exec.Command("go", []string{
+	//buildCMD := exec.Command("go", []string{
 	//	"run", "./...", "build",
 	//	"-gitsha", "devel",
 	//	"-turbine-core-server", grpcListenAddress,
 	//	"-app-path", t.appPath,
 	//}...)
+	if err := t.build(ctx); err != nil {
+		return fmt.Errorf("couldn't build Java app: %w", err)
+	}
 
-	cmd := exec.Command("", grpcListenAddress)
+	return turbine.RunCMD(ctx, t.logger, t.runCMD(ctx, grpcListenAddress))
+}
+
+func (t *turbineJavaCLI) build(ctx context.Context) error {
+	cmd := exec.Command(
+		"mvn",
+		"-U",
+		"clean",
+		"package",
+		"-Dquarkus.package.type=uber-jar",
+	)
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Dir = t.appPath
 	cmd.Env = os.Environ()
 
-	return turbine.RunCMD(ctx, t.logger, cmd)
+	if err := cmd.Run(); err != nil {
+		t.logger.Errorf(ctx, err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func (t *turbineJavaCLI) runCMD(ctx context.Context, grpcAddress string) *exec.Cmd {
+	split := strings.Split(grpcAddress, ":")
+	cmd := exec.Command(
+		"java",
+		"-jar",
+		"-Dquarkus.grpc.server.port="+split[1],
+		"target/turbine-java-example-1.0-SNAPSHOT-runner.jar",
+	)
+
+	t.logger.Infof(ctx, "java run cmd: %v", cmd.String())
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Dir = t.appPath
+	cmd.Env = os.Environ()
+
+	return cmd
 }
