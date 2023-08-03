0b403a879aac74e2a508aadc7f28a55cae611588
Eric Cheatham <eric@meroxa.io>
2023-08-03 15:10:22 -0400

    Add context for design decisions and future work

--- cmd/meroxa/flink/deploy.go
+++ cmd/meroxa/flink/deploy.go
@@ -27,6 +27,8 @@ const (
 )
 
 func doesJobUseMeroxaPlatform(ctx context.Context, jarPath string) (bool, error) {
+	//TODO: This approach is fine for now but may prove to be too naive in the future
+	// https://github.com/meroxa/product/issues/953
 	cmd := exec.CommandContext(ctx, "jar", "-tf", jarPath)
 	output, err := cmd.Output()
 	if err != nil {
@@ -57,74 +59,77 @@ func GetIRSpec(ctx context.Context, jarPath string, secrets map[string]string, l
 		return nil, err
 	}
 
-	if usesMeroxa {
-		cwd, err := os.Getwd()
-		if err != nil {
-			return nil, err
-		}
-		irFilepath := filepath.Join(cwd, irFilename)
-
-		cmd := exec.CommandContext(ctx, "java", "-jar", jarPath)
-		cmd.Env = append(
-			cmd.Environ(),
-			fmt.Sprintf("%s=%s", modeEnvVar, irVal),
-			fmt.Sprintf("%s=%s", outputEnvVar, irFilename))
-		output, err := cmd.CombinedOutput() // all java output goes to stderr, so that's fun
-		defer func() {
-			_ = os.Remove(irFilepath)
-		}()
-		if err != nil {
-			return nil, fmt.Errorf("%s\n%v", output, err)
-		}
-
-		_, err = os.Stat(irFilepath)
-		if err != nil {
-			// @TODO try the docker way because the jar is skinny
-			// Otherwise, there are no Meroxa* classes in this main class
-			return nil, nil
-		}
-
-		b, err := os.ReadFile(irFilepath)
-		if err != nil {
-			return nil, err
-		}
-
-		// @TODO assess the scope of updating validateCollections to use the ConnectorSpec
-		var spec ir.DeploymentSpec
-		if err = json.Unmarshal(b, &spec); err != nil {
-			return nil, err
-		}
-
-		spec.Secrets = secrets
-		// workarounds to validate spec
-		spec.Definition.Metadata.SpecVersion = ir.LatestSpecVersion
-		spec.Definition.Metadata.Turbine.Language = "js" // java and flink are not acceptable yet
-		spec.Definition.Metadata.Turbine.Version = majorJavaVersion
-
-		// hardcode all sources to one destination as streams
-		destinationUUID := ""
-		var sourceUUIDs []string
-		for _, cs := range spec.Connectors {
-			if cs.Type == ir.ConnectorDestination {
-				destinationUUID = cs.UUID
-			} else {
-				sourceUUIDs = append(sourceUUIDs, cs.UUID)
-			}
-		}
-
-		for _, u := range sourceUUIDs {
-			ss := ir.StreamSpec{
-				UUID:     uuid.New().String(),
-				FromUUID: u,
-				ToUUID:   destinationUUID,
-				Name:     u + "_" + destinationUUID,
-			}
-			spec.Streams = append(spec.Streams, ss)
-		}
-		return &spec, nil
+	if !usesMeroxa {
+		return nil, nil
 	}
 
-	return nil, nil
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+	irFilepath := filepath.Join(cwd, irFilename)
+
+	// The submitted jar is executed with some special env vars set to inform the `MeroxaExecutionEnvironment` to
+	// short circuit execution and emit an IR spec instead https://github.com/meroxa/flink-platform-prototype/blob/main/src/main/java/com/meroxa/flink/MeroxaExecutionEnvironment.java#L64-L69
+	cmd := exec.CommandContext(ctx, "java", "-jar", jarPath)
+	cmd.Env = append(
+		cmd.Environ(),
+		fmt.Sprintf("%s=%s", modeEnvVar, irVal),
+		fmt.Sprintf("%s=%s", outputEnvVar, irFilename))
+	output, err := cmd.CombinedOutput() // all java output goes to stderr, so that's fun
+	defer func() {
+		_ = os.Remove(irFilepath)
+	}()
+	if err != nil {
+		return nil, fmt.Errorf("%s\n%v", output, err)
+	}
+
+	_, err = os.Stat(irFilepath)
+	if err != nil {
+		// @TODO try the docker way because the jar is skinny
+		// Otherwise, there are no Meroxa* classes in this main class
+		return nil, nil
+	}
+
+	b, err := os.ReadFile(irFilepath)
+	if err != nil {
+		return nil, err
+	}
+
+	// @TODO assess the scope of updating validateCollections to use the ConnectorSpec
+	var spec ir.DeploymentSpec
+	if err = json.Unmarshal(b, &spec); err != nil {
+		return nil, err
+	}
+
+	spec.Secrets = secrets
+	// workarounds to validate spec
+	spec.Definition.Metadata.SpecVersion = ir.LatestSpecVersion
+	spec.Definition.Metadata.Turbine.Language = "js" // java and flink are not acceptable yet
+	spec.Definition.Metadata.Turbine.Version = majorJavaVersion
+
+	// hardcode all sources to one destination as streams
+	destinationUUID := ""
+	var sourceUUIDs []string
+	for _, cs := range spec.Connectors {
+		if cs.Type == ir.ConnectorDestination {
+			destinationUUID = cs.UUID
+		} else {
+			sourceUUIDs = append(sourceUUIDs, cs.UUID)
+		}
+	}
+
+	for _, u := range sourceUUIDs {
+		ss := ir.StreamSpec{
+			UUID:     uuid.New().String(),
+			FromUUID: u,
+			ToUUID:   destinationUUID,
+			Name:     u + "_" + destinationUUID,
+		}
+		spec.Streams = append(spec.Streams, ss)
+	}
+	return &spec, nil
+
 }
 
 func verifyJavaVersion(ctx context.Context, l log.Logger) {
